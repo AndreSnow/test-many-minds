9cf2920506718e4169610cb9692263374de8877b
Andre Neves <andre_neve@hotmail.com>
2023-03-25 17:06:46 -0300

    feat(app): criation components of pages

---
+++ application/views/footer.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+</body>
+<footer>
+    <div class="card" style="bottom: 0;
+        position: fixed;
+        width: 90%;
+        margin: 0 auto;
+        margin-left: 5%;
+        text-align: center;">
+        <div class="card-body">
+            <h5 class="card-title">O seu dashboard de todas as horas</h5>
+            <p class="card-text">Desenvolvido por
+                <a href="www.github.com/AndreSnow">André Neves
+                    <i class="fa-brands fa-github" style="color: #000000;"></i></a>
+            </p>
+        </div>
+    </div>
+</footer>
+
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta content-type='text/html' charset="utf-8">
+
+    <title><?php echo title() ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
